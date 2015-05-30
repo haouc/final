3a70199cfdd3b42746752b1f8905273510c5cf6e
haouc <haozhou@uchicago.edu>
2015-05-30 01:13:05 -0500

    Working on Message
    05/30/2015 1:12AM

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,19 @@
 class MessagesController < ApplicationController
 
+	before_action :find_user
+	def find_user
+		if (!session["user_id"].present?)
+			redirect_to login_path
+		end
+	end
+
+
 	def index
 		@user = User.find_by_id(session["user_id"])
+		if params["user_id"].present?
+			@messages = Message.where(sender_id: params["user_id"])
+		end
+		@messages = @messages.order('date desc').paginate(:per_page => 5, :page => params[:page])
 	end
 
 	def new
@@ -9,7 +21,14 @@ class MessagesController < ApplicationController
 	end
 
 	def create
-		
+		message = Message.new
+		message.receiver_id = params[:receiver]
+		message.content = params[:message]
+		message.date = DateTime.now.to_i
+		message.sender_id = session["user_id"]
+		message.is_read = false
+		message.save
+		redirect_to root_path
 	end
 
 	def destroy
